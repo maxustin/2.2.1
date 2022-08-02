d822470415431e87cde2009dcd1f8a94f74e6657
maxustin <99762254+maxustin@users.noreply.github.com>
2022-08-02 09:12:24 +0300

    Add files via upload

---
+++ PP_2_2_1_spring_hibernate/2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -0,0 +1,13 @@
+package hiber.service;
+
+import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+    List<User> listUsers();
+
+    User getUserByCarInfo(int series, String model);
+}
